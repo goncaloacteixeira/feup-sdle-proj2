678565476c797a5a1c61b785d2b69b3aeab477a6
Gonçalo Teixeira <goncalo.ac.teixeira@gmail.com>
2021-12-20 14:38:09 +0000

    added boilerplate for expressjs and react

---
+++ src/client/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/test")
+        .then((res) => res.json())
+        .then((data) => {
+            setData(data);
+            console.log(data)
+        });
+  }, []);
+
+  return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{!data ? "Loading..." : data}</p>
+        </header>
+      </div>
+  );
+}
+
+export default App;

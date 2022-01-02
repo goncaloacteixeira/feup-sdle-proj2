5e449e7549928dcdf6550d82bbc9a5befd717c6c
Gonçalo Teixeira <goncalo.ac.teixeira@gmail.com>
2022-01-02 22:56:42 +0000

    bug fix and some UX enhancements

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,38 +1,44 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import MainPage from './pages/MainPage';
-import ProfilePage from './pages/ProfilePage';
+import MainPage from "./pages/MainPage";
+import ProfilePage from "./pages/ProfilePage";
 
-import {auth} from './fire.js';
+import { auth } from "./fire.js";
 
 import WelcomePage from "./pages/WelcomePage";
+import { CircularProgress } from "@mui/material";
+import LoadingPage from "./pages/LoadingPage";
 
 function App() {
-    const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  const [isLoggedIn, setIsLoggedIn] = React.useState(null);
 
-    auth.onAuthStateChanged((user) => {
-        return user ? setIsLoggedIn(true) : setIsLoggedIn(false);
-    });
+  auth.onAuthStateChanged((user) => {
+    return user ? setIsLoggedIn(true) : setIsLoggedIn(false);
+  }, []);
 
-    return (
-        <div className="App">
-            {!isLoggedIn ?
-                <Router>
-                    <Routes>
-                        <Route path="/" element={<WelcomePage/>}/>
-                    </Routes>
-                </Router>
-                :
-                <Router>
-                    <Routes>
-                        <Route path="/" element={<MainPage/>}/>
-                        <Route path="/profile/:username" element={<ProfilePage/>}/>
-                    </Routes>
-                </Router>
-            }
-        </div>
-    );
+  if (isLoggedIn == null) {
+    return <LoadingPage />;
+  }
+
+  return (
+    <div className="App">
+      {!isLoggedIn ? (
+        <Router>
+          <Routes>
+            <Route path="/" element={<WelcomePage />} />
+          </Routes>
+        </Router>
+      ) : (
+        <Router>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/profile/:username" element={<ProfilePage />} />
+          </Routes>
+        </Router>
+      )}
+    </div>
+  );
 }
 
 export default App;

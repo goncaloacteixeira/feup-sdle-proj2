d4e5048730379e93fd07a4edbec2c878d52e2d2f
Gonçalo Teixeira <goncalo.ac.teixeira@gmail.com>
2022-01-06 16:08:11 +0000

    landing page done

--- src/client/src/pages/LandingPage.js
+++ src/client/src/pages/LandingPage.js
@@ -1,4 +1,4 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid, Typography, Link } from '@mui/material';
 import Background from '../bg.png'
 
 
@@ -13,11 +13,26 @@ export default function LandingPage() {
 
     return (
         <div style={style}>
-            <Grid container direction="column">
+            <Grid alignItems="center" justifyContent="center" style={{height: '100%'}} container direction="column">
                 <Grid item>
                     <Typography variant="h1" color="white">
                         Tuiter
                     </Typography>
+                </Grid>
+                <Grid item>
+                    <Typography mt={5} variant="h5" color="white">
+                        Tuiter is a distributed peer-to-peer social network built on libp2p-js 
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Grid mt={10} container spacing={5}>
+                        <Grid item>
+                            <Link href="/login" variant="h5" style={{color: 'white'}}>Login</Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="/signup" variant="h5" style={{color: 'white'}}>Sign Up</Link>
+                        </Grid>
+                    </Grid>
                 </Grid>
 
             </Grid> 

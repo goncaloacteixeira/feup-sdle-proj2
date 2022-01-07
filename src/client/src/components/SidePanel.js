c7105ddabb2f87202267deb69dc16809d84a6c74
Gonçalo Teixeira <goncalo.ac.teixeira@gmail.com>
2022-01-07 12:01:51 +0000

    side panel users done

--- src/client/src/components/SidePanel.js
+++ src/client/src/components/SidePanel.js
@@ -1,156 +1,129 @@
 import React from "react";
-import { TextField, InputAdornment } from "@mui/material";
+import {TextField, InputAdornment} from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
 
 import {
-  Grid,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemAvatar,
-  Avatar,
-  ListItemText,
-  Button,
-  Link,
-  CircularProgress,
-  Typography,
+    Grid,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemAvatar,
+    Avatar,
+    ListItemText,
+    Button,
+    Link,
+    CircularProgress,
+    Typography,
 } from "@mui/material";
 import axios from "axios";
+import UserCard from "./UserCard";
 
 export default function SidePanel() {
-  const [usernames, setUsernames] = React.useState(null);
-  const [checkedState, setCheckedState] = React.useState(null);
-  const [info, setInfo] = React.useState(null);
+    const [usernames, setUsernames] = React.useState(null);
+    const [checkedState, setCheckedState] = React.useState(null);
+    const [info, setInfo] = React.useState(null);
 
-  React.useEffect(() => {
-    axios.get("/p2p/info").then((res) => setInfo(res.data));
-  }, []);
+    React.useEffect(() => {
+        axios.get("/p2p/info").then((res) => setInfo(res.data));
+    }, []);
 
-  React.useEffect(() => {
-    axios.get("/users").then((res) => setUsernames(res.data));
-  }, [info]);
+    React.useEffect(() => {
+        axios.get("/users").then((res) => setUsernames(res.data));
+    }, [info]);
 
-  React.useEffect(() => {
-    if (!info) return;
-    removeSelf();
-    setCheckedState(makeState());
-  }, [usernames]);
+    React.useEffect(() => {
+        if (!info) return;
+        removeSelf();
+        setCheckedState(makeState());
+    }, [usernames]);
 
-  const removeSelf = () => {
-    const index = usernames.indexOf(info.data.username);
-    if (index > -1) {
-      usernames.splice(index, 1);
-    }
-  };
-
-  const makeState = () => {
-    let state = new Array(usernames.length).fill(false);
-    usernames.map((node, index) => {
-      info.data.subscribed.map((n) => {
-        if (node === n) {
-          state[index] = true;
+    const removeSelf = () => {
+        const index = usernames.indexOf(info.data.username);
+        if (index > -1) {
+            usernames.splice(index, 1);
         }
-      });
-    });
-    return state;
-  };
+    };
 
-  const handleClick = (e, position) => {
-    e.preventDefault();
-    const updatedCheckedState = checkedState.map((item, index) =>
-      index === position ? !item : item
-    );
-
-    if (!checkedState[position]) {
-      axios.post("/p2p/subscribe", { username: e.target.id }).then((res) => {
-        if (res.status === 200) {
-          setCheckedState(updatedCheckedState);
-        }
-      });
-    } else {
-      axios.post("/p2p/unsubscribe", { username: e.target.id }).then((res) => {
-        if (res.status === 200) {
-          setCheckedState(updatedCheckedState);
-        }
-      });
-    }
-  };
-
-  const handleSearchChange = (e) => {
-    console.log(e.target.value);
-
-    axios.get("/users/" + e.target.value).then((res) => setUsernames(res.data));
-  }
-
-  return (
-    <Grid container alignItems="center">
-      <Typography mx={2} variant="h4">
-          Users
-      </Typography>
-      <Grid item>
-      <TextField 
-          size="small"
-          fullWidth
-          placeholder="Search users"
-          id="outlined-start-adornment"
-          sx={{ m: 1, width: '25ch' }}
-          onChange={handleSearchChange}
-          InputProps={{
-            startAdornment: <InputAdornment position="start"><SearchIcon /></InputAdornment>,
-          }}
-        />
-      </Grid>
-      {!info || !usernames || !checkedState ? (
-        <CircularProgress />
-      ) : (
-        <List
-          dense
-          sx={{
-            width: "100%",
-            maxWidth: 360,
-            bgcolor: "background.paper",
-            position: "relative",
-            overflow: "auto",
-            maxHeight: 400,
-            "& ul": { padding: 0 },
-          }}
-        >
-          {usernames.map((value, index) => {
-            const labelId = `checkbox-list-secondary-label-${value}`;
-            return (
-              <ListItem
-                key={value}
-                secondaryAction={
-                  <Button
-                    variant={checkedState[index] ? "outlined" : "contained"}
-                    onClick={(e) => handleClick(e, index)}
-                    size="small"
-                    id={value}
-                  >
-                    {checkedState[index] ? "Following" : "Follow"}
-                  </Button>
+    const makeState = () => {
+        let state = new Array(usernames.length).fill(false);
+        usernames.map((node, index) => {
+            info.data.subscribed.map((n) => {
+                if (node === n) {
+                    state[index] = true;
                 }
-                disablePadding
-              >
-                <ListItemButton>
-                  <ListItemAvatar>
-                    <Avatar>{value[0].toUpperCase()}</Avatar>
-                  </ListItemAvatar>
-                  <ListItemText id={labelId}>
-                    <Link
-                      href={"/profile/" + value}
-                      color="inherit"
-                      underline="hover"
-                    >
-                      {value}
-                    </Link>
-                  </ListItemText>
-                </ListItemButton>
-              </ListItem>
-            );
-          })}
-        </List>
-      )}
-    </Grid>
-  );
+            });
+        });
+        return state;
+    };
+
+    const handleClick = (e, position) => {
+        e.preventDefault();
+        const updatedCheckedState = checkedState.map((item, index) =>
+            index === position ? !item : item
+        );
+
+        if (!checkedState[position]) {
+            axios.post("/p2p/subscribe", {username: e.target.id}).then((res) => {
+                if (res.status === 200) {
+                    setCheckedState(updatedCheckedState);
+                }
+            });
+        } else {
+            axios.post("/p2p/unsubscribe", {username: e.target.id}).then((res) => {
+                if (res.status === 200) {
+                    setCheckedState(updatedCheckedState);
+                }
+            });
+        }
+    };
+
+    const handleSearchChange = (e) => {
+        console.log(e.target.value);
+
+        axios.get("/users/" + e.target.value).then((res) => setUsernames(res.data));
+    }
+
+    return (
+        <Grid container direction="column">
+            <Grid item>
+                <Grid container justifyContent="space-between" alignItems="center">
+                    <Grid item>
+                        <Typography variant="h4">
+                            Users
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <TextField
+                            size="small"
+                            fullWidth
+                            placeholder="Search users"
+                            id="outlined-start-adornment"
+                            sx={{m: 1}}
+                            onChange={handleSearchChange}
+                            InputProps={{
+                                startAdornment: <InputAdornment position="start"><SearchIcon/></InputAdornment>,
+                            }}
+                        />
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item>
+                {!info || !usernames || !checkedState ?
+                    <CircularProgress/> :
+                    (
+                        <Grid container my={2} spacing={2} direction="column">
+                            {usernames.map((value, index) => {
+                                return (
+                                    <Grid item key={value}>
+                                        <UserCard handler={(e) => handleClick(e, index)} username={value}
+                                                  checked={checkedState[index]}/>
+                                    </Grid>
+                                )
+                            })}
+                        </Grid>
+                    )
+                }
+            </Grid>
+        </Grid>
+    );
 }
--- src/client/src/pages/ProfilePage.js
+++ src/client/src/pages/ProfilePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 
 import "../styles/profile.css";
 import CustomAppBar from "../components/AppBar";
@@ -8,102 +8,102 @@ import ProfileOffline from "../components/ProfileOffline";
 import ProfileSelf from "../components/ProfileSelf";
 import Profile from "../components/Profile";
 import SidePanel from "../components/SidePanel";
-import { Grid } from "@mui/material";
+import {Grid} from "@mui/material";
 import LoadingPage from "./LoadingPage";
 import ProfileNotFound from "../components/ProfileNotFound";
 
 export default function ProfilePage() {
-  const { username } = useParams();
+    const {username} = useParams();
 
-  const [data, setData] = React.useState(null);
+    const [data, setData] = React.useState(null);
 
-  React.useEffect(() => {
-    fetch("/p2p/profiles/" + username)
-      .then((res) => res.json())
-      .then((res) => {
-        setData({
-          status: res.message.message,
-          record: res.message.content,
-        });
-      });
-  }, []);
+    React.useEffect(() => {
+        fetch("/p2p/profiles/" + username)
+            .then((res) => res.json())
+            .then((res) => {
+                setData({
+                    status: res.message.message,
+                    record: res.message.content,
+                });
+            });
+    }, []);
 
-  if (!data) {
-    return <LoadingPage />;
-  }
+    if (!data) {
+        return <LoadingPage/>;
+    }
 
-  switch (data.status) {
-    case "ERR_NOT_FOUND":
-      return (
-        <div className="ProfilePage">
-          <CustomAppBar />
-          <Grid container>
-            <Grid item p={4} xs={3}>
-              <SidePanel />
-            </Grid>
-            <Grid p={4} item xs={9}>
-            <ProfileNotFound username={username} />
-            </Grid>
-          </Grid>
-        </div>
-      )
-    case "ERR_SELF":
-      return (
-        <div className="ProfilePage">
-          <CustomAppBar />
-          <Grid container>
-            <Grid item p={4} xs={3}>
-              <SidePanel />
-            </Grid>
-            <Grid p={4} item xs={9}>
-              <ProfileSelf username={username} data={data} />
-            </Grid>
-          </Grid>
-        </div>
-      );
-    case "ERR_NOT_SUBSCRIBED":
-      return (
-        <div className="ProfilePage">
-          <CustomAppBar />
-          <Grid container>
-            <Grid item p={4} xs={3}>
-              <SidePanel />
-            </Grid>
-            <Grid p={4} item xs={9}>
-            <ProfileUnsub username={username} />
-            </Grid>
-          </Grid>
-        </div>
-      );
-    case "ERR_NO_INFO":
-      return (
-        <div className="ProfilePage">
-          <CustomAppBar />
-          <Grid container>
-            <Grid item p={4} xs={3}>
-              <SidePanel />
-            </Grid>
-            <Grid p={4} item xs={9}>
-            <ProfileOffline username={username} />
-            </Grid>
-          </Grid>          
-        </div>
-      );
-    case "OK":
-      return (
-        <div className="ProfilePage">
-          <CustomAppBar />
-          <Grid container>
-            <Grid item p={4} xs={3}>
-              <SidePanel />
-            </Grid>
-            <Grid p={4} item xs={9}>
-            <Profile username={username} data={data} />
-            </Grid>
-          </Grid>
-        </div>
-      );
-    default:
-      return <h1>ERROR</h1>;
-  }
+    switch (data.status) {
+        case "ERR_NOT_FOUND":
+            return (
+                <div className="ProfilePage">
+                    <CustomAppBar/>
+                    <Grid container>
+                        <Grid item p={4} xs={4}>
+                            <SidePanel/>
+                        </Grid>
+                        <Grid p={4} item xs={8}>
+                            <ProfileNotFound username={username}/>
+                        </Grid>
+                    </Grid>
+                </div>
+            )
+        case "ERR_SELF":
+            return (
+                <div className="ProfilePage">
+                    <CustomAppBar/>
+                    <Grid container>
+                        <Grid item p={4} xs={4}>
+                            <SidePanel/>
+                        </Grid>
+                        <Grid p={4} item xs={8}>
+                            <ProfileSelf username={username} data={data}/>
+                        </Grid>
+                    </Grid>
+                </div>
+            );
+        case "ERR_NOT_SUBSCRIBED":
+            return (
+                <div className="ProfilePage">
+                    <CustomAppBar/>
+                    <Grid container>
+                        <Grid item p={4} xs={4}>
+                            <SidePanel/>
+                        </Grid>
+                        <Grid p={4} item xs={8}>
+                            <ProfileUnsub username={username}/>
+                        </Grid>
+                    </Grid>
+                </div>
+            );
+        case "ERR_NO_INFO":
+            return (
+                <div className="ProfilePage">
+                    <CustomAppBar/>
+                    <Grid container>
+                        <Grid item p={4} xs={4}>
+                            <SidePanel/>
+                        </Grid>
+                        <Grid p={4} item xs={8}>
+                            <ProfileOffline username={username}/>
+                        </Grid>
+                    </Grid>
+                </div>
+            );
+        case "OK":
+            return (
+                <div className="ProfilePage">
+                    <CustomAppBar/>
+                    <Grid container>
+                        <Grid item p={4} xs={4}>
+                            <SidePanel/>
+                        </Grid>
+                        <Grid p={4} item xs={8}>
+                            <Profile username={username} data={data}/>
+                        </Grid>
+                    </Grid>
+                </div>
+            );
+        default:
+            return <h1>ERROR</h1>;
+    }
 }

aa4c6d7f7d32cdcb9fbcc51e6fa9afb2315a6574
Gonçalo Pereira <goncalo.valpneus@hotmail.com>
2022-01-08 17:58:27 +0000

    Post card done

--- src/client/src/components/Post.js
+++ src/client/src/components/Post.js
@@ -1,26 +1,32 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import {CardHeader, Grid} from "@mui/material";
+import LetterAvatar from "./LetterAvatar";
 
 export default function Post(props) {
     const getDate = (timestamp) => {
         const date = new Date(timestamp);
-        return date.toString();
-    }
+        const timeComponents = [date.getHours(), date.getMinutes()];
+        const aux = timeComponents
+            .map(component => {
+                const pad = (component < 10) ? '0' : '';
+                return pad + component;
+            })
+            .join(':');
+        return date.toLocaleDateString() +" "+ aux;
+    };
 
     return (
-        <Card sx={{ minWidth: 275 }}>
+        <Card sx={{minWidth: 275}}>
+            <CardHeader
+                avatar={<LetterAvatar name={props.author}/>}
+                title={<Typography style={{ fontWeight: 600 }} color="primary" variant="h6">{props.author}</Typography>}
+                action={getDate(props.timestamp)}
+            />
             <CardContent>
-                <Typography variant="h6">
-                    {props.author}
-                </Typography>
-                <Typography variant="subtitle2">
-                    {getDate(props.timestamp)}
-                </Typography>
-                <Typography variant="body1">
-                    {props.content}
-                </Typography>
+                <Typography variant="body2">{props.content}</Typography>
             </CardContent>
         </Card>
     );
